95740f61435f9d50ab60d65fa66f2a28426592b3
JanluPaulsenEpiUse <janlu.paulsen@epiuse.com>
2026-05-06 13:28:47 +0000

    dropped the auto creation of delivery note

--- ecommerce_integrations/shopify/fulfillment.py
+++ ecommerce_integrations/shopify/fulfillment.py
@@ -1,15 +1,8 @@
-from copy import deepcopy
-
 import frappe
-from erpnext.selling.doctype.sales_order.sales_order import make_delivery_note
-from frappe.utils import cint, cstr, getdate
+from erpnext.selling.doctype.sales_order.sales_order import create_pick_list
+from frappe.utils import cint, cstr
 
-from ecommerce_integrations.shopify.constants import (
-	FULLFILLMENT_ID_FIELD,
-	ORDER_ID_FIELD,
-	ORDER_NUMBER_FIELD,
-	SETTING_DOCTYPE,
-)
+from ecommerce_integrations.shopify.constants import SETTING_DOCTYPE
 from ecommerce_integrations.shopify.order import get_sales_order
 from ecommerce_integrations.shopify.utils import create_shopify_log
 
@@ -36,51 +29,22 @@ def create_delivery_note(shopify_order, setting, so):
 	if not cint(setting.sync_delivery_note):
 		return
 
-	for fulfillment in shopify_order.get("fulfillments"):
-		if (
-			not frappe.db.get_value("Delivery Note", {FULLFILLMENT_ID_FIELD: fulfillment.get("id")}, "name")
-			and so.docstatus == 1
-		):
-			dn = make_delivery_note(so.name)
-			setattr(dn, ORDER_ID_FIELD, fulfillment.get("order_id"))
-			setattr(dn, ORDER_NUMBER_FIELD, shopify_order.get("name"))
-			setattr(dn, FULLFILLMENT_ID_FIELD, fulfillment.get("id"))
-			dn.set_posting_time = 1
-			dn.posting_date = getdate(fulfillment.get("created_at"))
-			dn.naming_series = setting.delivery_note_series or "DN-Shopify-"
-			dn.items = get_fulfillment_items(
-				dn.items, fulfillment.get("line_items"), fulfillment.get("location_id")
-			)
-			dn.flags.ignore_mandatory = True
-			dn.save()
-			dn.submit()
-
-			if shopify_order.get("note"):
-				dn.add_comment(text=f"Order Note: {shopify_order.get('note')}")
+	if shopify_order.get("fulfillments") and so.docstatus == 1:
+		create_draft_pick_list(so)
 
 
-def get_fulfillment_items(dn_items, fulfillment_items, location_id=None):
-	# local import to avoid circular imports
-	from ecommerce_integrations.shopify.product import get_item_code
+def create_draft_pick_list(so):
+	if get_existing_pick_list(so.name):
+		return
 
-	fulfillment_items = deepcopy(fulfillment_items)
+	pick_list = create_pick_list(so.name)
+	pick_list.flags.ignore_mandatory = True
+	pick_list.insert(ignore_permissions=True)
 
-	setting = frappe.get_cached_doc(SETTING_DOCTYPE)
-	wh_map = setting.get_integration_to_erpnext_wh_mapping()
-	warehouse = wh_map.get(str(location_id)) or setting.warehouse
 
-	final_items = []
-
-	def find_matching_fullfilement_item(dn_item):
-		nonlocal fulfillment_items
-
-		for item in fulfillment_items:
-			if get_item_code(item) == dn_item.item_code:
-				fulfillment_items.remove(item)
-				return item
-
-	for dn_item in dn_items:
-		if shopify_item := find_matching_fullfilement_item(dn_item):
-			final_items.append(dn_item.update({"qty": shopify_item.get("quantity"), "warehouse": warehouse}))
-
-	return final_items
+def get_existing_pick_list(sales_order):
+	return frappe.db.get_value(
+		"Pick List Item",
+		{"sales_order": sales_order, "docstatus": ("<", 2)},
+		"parent",
+	)
